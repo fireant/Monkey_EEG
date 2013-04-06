8edc991fd9762092720603e67b6229d3d0703e7e
leonpalafox <leonoel49@gmail.com>
2013-04-05 18:10:08 -0700

    Documentation

--- PLRegress/rearrangeData.m
+++ PLRegress/rearrangeData.m
@@ -1,7 +1,8 @@
-%Clear all the previous data
+%Clear all the previous data from the workspace
 clear
 %Load the motion matrix as in the downloaded files in the Wiki
 %You have to change the location of the files depending on where you have them
+%This first line loads the motion data
 load('/home/leon/Data/Monkey/20090611S1_FTT_A_ZenasChao_mat_ECoG32-Motion12/Motion.mat')
 %Specify the number of channels
 NoChannels=32
@@ -9,7 +10,7 @@ NoChannels=32
 MasterArray=[]
 %This part iterates over all the files and append them in MasterArray, these files have been previously processed by 
 for i=1:NoChannels
-        direct='/home/leon/Data/Monkey/Features/features_ch';
+        direct='/home/leon/Data/Monkey/50ms/features_ch';
         infile=strcat(direct,num2str(i),'.mat'); %This just createes the dynamic filenames (infile)
         load(infile);
         for j=1:length(featTime)
@@ -25,20 +26,26 @@ for i=7:7
         NewData=[NewData MotionData{i}];
 end
 %Normalization of newdata
-Mean=mean(NewData);
-Std=std(NewData);
-[time,dims]=size(NewData);
-NewData=NewData-repmat(Mean,time,1);
-NewData=NewData./repmat(Std,time,1);
-for i=1:1036
-eegT(i)=featTime{i,2};
+%The normalization of the data is done substractin the mean and dividing by the Standard Deviation, as donde in the paper
+%Other Normalization methods could also be used
+Mean=mean(NewData);%Calculate the mean
+Std=std(NewData);%Calculate the standard deviation
+[time,dims]=size(NewData);%get the properties of the data
+NewData=NewData-repmat(Mean,time,1); %rest the mean
+NewData=NewData./repmat(Std,time,1); %rest the stdv
+for i=1:length(featTime) %iterate over all the cells in the featime structure
+        eegT(i)=featTime{i,2}; %Extract the timestamp from the cells
 end
-[trash startIdx]=min(abs(eegT));
-eegT=eegT(startIdx:end);
+[trash startIdx]=min(abs(eegT));%Obtain the minimum timestamp
+%The minimum timestamp has to be done, since the EEG starts before the motion, 
+%the motion starts at time 0, but the EEG has not ime 0 
+%startidx contains the first timestamp
+eegT=eegT(startIdx:end);%rearrange the array
+MasterArray=MasterArray(startIdx:end,:);%Master array need to be rezised as well
 for i=1:length(eegT)
-        [mi midx]=min(abs(MotionTime-eegT(i)));
-        MIdx(i)=midx;
+        [mi midx]=min(abs(MotionTime-eegT(i)));%Find the closest timestamp in motion to the one in the EEG
+        MIdx(i)=midx;%Create an array with all the indexes of the motion
         NewMotion(i)=MotionTime(midx);
 end
-NewMotionData=NewData(MIdx,:);
-MasterArray=MasterArray(startIdx:end,:);
+NewMotionData=NewData(MIdx,:);%Use the indexes to get the correspondent motion array
+
