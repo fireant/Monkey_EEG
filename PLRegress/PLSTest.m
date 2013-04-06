69f458f7b5e67c65df60338c33bf73e8d5a2ae2f
leonpalafox <leonoel49@gmail.com>
2013-04-05 19:05:36 -0700

    Documentation

--- PLRegress/PLSTest.m
+++ PLRegress/PLSTest.m
@@ -1,5 +1,5 @@
 
-NoComponents=75;%NoCOmponents states the final number of components 
+NoComponents=30;%NoCOmponents states the final number of components 
 %MasterArray contains a Tx3200 Feature matrix (Channelsx10x10)
 X=MasterArray;
 %NewMotionData has the normalized and aligned Motion Data from rearrandeData.m
@@ -10,7 +10,7 @@ y=NewMotionData;
 %Here we generate the crossvalidated sets
 %indices = crossvalind('Kfold',ny,10);
 indices(1:n)=0;
-indices(1:ceil(2*n/3))=1; %I've used 2/3ds of the data for training, rest for testing
+%indices(1:ceil(1*n/5))=1; %I've used 1/3d of the data for testing, rest for training
 test = (indices == 1); train = ~test;
 
 %plsregress is the matlab function that performs PLS regression 
@@ -19,16 +19,21 @@ test = (indices == 1); train = ~test;
 for i=2:NoComponents
         [Xloadings,Yloadings,Xscores,Yscores,betaPLS,pctVar,PLSmsep] = plsregress(X(train,:),y(train,:),i);
         %To do prediction, we need to append a vector of 1s since the matrix beta is an augmented matrix to account for the affine factor of the regression
-        yfitPLS = [ones(sum(test),1) X(test,:)]*betaPLS;
-        TSS = sum((y(test,:)-repmat(mean(y(test,:)),sum(test),1)).^2);
-        RSS_PLS = sum((y(test,:)-yfitPLS).^2);
-        rsquaredPLS(i-1) = 1 - RSS_PLS/TSS
+        yfitPLS = [ones(sum(train),1) X(train,:)]*betaPLS;
+        TSS = sum((y(train,:)-repmat(mean(y(train,:)),sum(train),1)).^2);
+        RSS_PLS = sum((y(train,:)-yfitPLS).^2);
+        %yfitPLS = [ones(sum(test),1) X(test,:)]*betaPLS;
+        %TSS = sum((y(test,:)-repmat(mean(y(test,:)),sum(test),1)).^2);
+        %RSS_PLS = sum((y(test,:)-yfitPLS).^2);
+        rsquaredPLS(i-1) = 1 - RSS_PLS/TSS;
 end
-
-plot(y(test,:),yfitPLS,'bo');
+figure
+%plot(y(test,:),yfitPLS,'bo');
+plot(y(train,:),yfitPLS,'bo');
 xlabel('Observed Response');
 ylabel('Fitted Response');
 legend({'PLSR with 20 Components'},'location','NW');
-
+figure
+plotyy([1:NoComponents-1],rsquaredPLS,[1:NoComponents-1],PLSmsep(2,3:end),'plot','plot')
 
 
